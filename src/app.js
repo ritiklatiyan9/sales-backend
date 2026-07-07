3b4f0232b7450491f652efbb69cd8b723eed5f51
[Ritik] <ritikkumar8077008067@gmail.com>
2026-07-08 00:52:06 +0530

    rg

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ const allowedCorsOrigins = new Set([
   'https://www.dgbooking.com',
   'https://dgbooking.com',
   'http://localhost:5173',
+  'http://localhost:5174',
   'http://localhost:3000',
 ]);
 
